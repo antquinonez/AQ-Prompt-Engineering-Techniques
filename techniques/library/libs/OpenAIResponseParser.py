72d828d3ddabb19681a63a94a131eeac43cc9e76
Antonio Quinonez <antquinonez@gmail.com>
2023-04-09 14:51:15 -0700

    Add dressing, cleanup

--- techniques/library/libs/OpenAIResponseParser.py
+++ techniques/library/libs/OpenAIResponseParser.py
@@ -1,14 +1,22 @@
-import openai
+# OpenAI Response Parser
+# The OpenAIResponseParser is a utility class designed to parse and store the responses from the OpenAI API, specifically for code generation tasks. The main goal is to provide a user-friendly way to access and analyze the content of the API responses, including code blocks and other relevant information.
+
+# Features
+# - Parse and store API response data in a pandas DataFrame for easy manipulation and analysis.
+# - Extract code blocks from the response content and store them separately.
+# - Apply styling to the DataFrame for better readability.
+
 import pandas as pd
-# Set display options
-pd.set_option('display.max_columns', None)  # Display all columns
-pd.set_option('display.expand_frame_repr', False)  # Prevent line break in DataFrame
-pd.set_option('display.max_colwidth', None)  # Display the full content of cells
+from typing import List, Dict, Any
 
 
 class OpenAIResponseParser:
-    def __init__(self, response):
+    def __init__(self, response: Dict[str, Any]) -> None:
+        """
+        Initialize OpenAIResponseParser with API response.
 
+        :param response: API response from OpenAI.
+        """
         self.response = response
 
         self.df = pd.DataFrame(
@@ -26,18 +34,22 @@ class OpenAIResponseParser:
         self._store_response()
         self._style()
 
-    def _style(self):
+    def _style(self) -> None:
+        """
+        Apply styling to the DataFrame.
+        """
         self.style = self.df.style.set_properties(**{'text-align': 'left'})
 
-
-    def _store_response(self):
+    def _store_response(self) -> None:
+        """
+        Store the response content into the DataFrame.
+        """
         content = self.response['choices'][0]['message']['content'].strip()
 
         import re
 
         pattern = re.compile(r"```python([\s\S]*?)```")
         code_blocks = re.findall(pattern, content)
-        # print ("code_blocks:", code_blocks)
 
         all_code = ""
         for code_block in code_blocks:
@@ -61,20 +73,10 @@ class OpenAIResponseParser:
             ignore_index=True,
         )
 
-        # print(self.df)
+    def get_dataframe(self) -> pd.DataFrame:
+        """
+        Get the DataFrame containing the API response.
 
-    def get_dataframe(self):
+        :return: DataFrame containing the API response.
+        """
         return self.df
-
-# Example usage:
-# api_key = "your_openai_api_key"
-# handler = OpenAIResponseHandler(api_key)
-
-# prompt1 = "Write a short summary about the history of computer programming."
-# handler.request_prompt(prompt1)
-
-# prompt2 = "Describe the main features of the Python programming language."
-# handler.request_prompt(prompt2)
-
-# df = handler.get_dataframe()
-# print(df)
